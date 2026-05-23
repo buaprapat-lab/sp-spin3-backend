35c41df77662b6df2a6648c84eba1e8c35dd2577
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-05-23 23:13:09 +0700

    rosolve miss routes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './configs/mongodb.js'
-
+import { router as apiRoutes } from './routes/index.js'
 
 dotenv.config()
 
@@ -10,11 +10,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
-
-
+app.use(express.json())
+app.use('/api', apiRoutes)
 
 await connectDB()
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
