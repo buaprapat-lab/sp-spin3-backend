911173edbb939203e8ee2c3d2190adcbed2cca5c
Korchat123 <korchat123@gmail.com>
2026-05-27 08:37:07 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -9,7 +9,10 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://sp-spin3-frontend.vercel.app/',
+  credentials: true
+}))
 app.use(express.json())
 app.use('/api', apiRoutes)
 
